066f9e63ca3f1dd2b0cf5254b417e6856e236b0b
sonali-rajput <sonalirajput1088@gmail.com>
2025-01-31 13:39:46 +0000

    add support for jdbc ... no more hard coded users

--- crud-implementation/src/main/java/com/luv2code/cruddemo/security/DemoSecurityConfig.java
+++ crud-implementation/src/main/java/com/luv2code/cruddemo/security/DemoSecurityConfig.java
@@ -2,13 +2,51 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for jdbc ... no more hard coded users
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use http basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable cross site request forgery (CSRF)
+        // In general not required for stateless rest api
+        http.csrf(csrf->csrf.disable());
+        return http.build();
+    }
+
+
+
+        /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -32,24 +70,5 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-        );
-
-        // use http basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-        // disable cross site request forgery (CSRF)
-        // In general not required for stateless rest api which uses POST, DELETE and/or PATCH
-        http.csrf(csrf->csrf.disable());
-        return http.build();
-    }
+*/
 }
